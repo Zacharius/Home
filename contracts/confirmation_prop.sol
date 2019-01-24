dc1b3aa168d1b6c1a1a213422e56ce9c6ade9c97
zacharius <zach@zfadd.is>
2019-01-24 17:39:28 -0600

    fixed errors, added functionality for contract interactions

--- contracts/confirmation_prop.sol
+++ contracts/confirmation_prop.sol
@@ -1,36 +1,37 @@
 pragma solidity ^0.5.0;
 
 import  "./home_base.sol";
+import "./home.sol";
 
 contract Confirmation_prop is Home_base {
 
-  public Proposal_Type type = Proposal_Type.Confirmation;
-  public bool open = true;
+  Proposal_Type public typeOf = Proposal_Type.Confirmation;
+  bool public open = true;
   Vote[] public votes;
-  public address home;
+  Home public home;
 
-  public uint pro = 0;
-  public uint con = 0;
+  uint public pro = 0;
+  uint public con = 0;
 
   constructor() public {
-    home = msg.sender;
+    home = Home(msg.sender);
   }
 
   function castVote(Vote_Type vote_type) public {
-    require(isMember(),
+    require(isMember(msg.sender),
 	    'Only members can vote');
 
-    vote_index = getPreviousVote(msg.sender);
+    int vote_index = getPreviousVote(msg.sender);
 
     //if voter has already voted, just change their previous vote
-    if(vote != -1){
-      changeVote(vote_index, vote_type);
+    if(vote_index != -1){
+      changeVote(uint(vote_index), vote_type);
       return;
      }
 
-    Vote vote;
+    Vote memory vote;
     vote.typeOf = vote_type;
-    vote.address = msg.sender;
+    vote.member = msg.sender;
     votes.push(vote);
 
     if(vote_type == Vote_Type.For)
@@ -38,13 +39,13 @@ contract Confirmation_prop is Home_base {
     if(vote_type == Vote_Type.Against)
       con++;
 
-    emit voteCast(vote, type, msg.sender, this);
+    emit voteCast(vote.typeOf, typeOf, msg.sender, address(this));
 
     tallyVote();
   }
 
   function tallyVote() internal {
-    total_members = getTotalMembers();
+    uint total_members = getTotalMembers();
 
     if( (pro/total_members) * 100 >= CONFIRMATION_PROP_THRESHOLD) {
       sendFinalResult();
@@ -52,43 +53,50 @@ contract Confirmation_prop is Home_base {
 
   }
 
-  function sendFinalResult() private {}
+  function sendFinalResult() internal {
+    home.receivePropResults();
+  }
 
-  function isMember() private returns(bool) {}
+  function isMember(address _member) internal returns(bool) {
+    return home.isMember(_member); 
+  }
 
-  function getTotalMembers() private return(uint) {}
+  function getTotalMembers() internal returns (uint) {
+    return home.getTotalMembers();
+  }
 
   //returns index of vote for voter, -1 if voter hasn't voted yet
-  function getPreviousVote(address voter) internal returns (uint) {
+  function getPreviousVote(address voter) internal returns (int) {
     for(uint i = 0; i<votes.length; i++){
-      vote = votes[i];
+      Vote storage vote = votes[i];
       if(vote.member == voter){
-	return i;
+	return int(i);
       }
     }
     return -1;
   }
 
-  function changeVote(uint vote_index, Vote_Type typeOf){
+  function changeVote(uint vote_index, Vote_Type vote_type) internal{
     bool change = false;
 
-    if( (votes[vote_index]).typeOf != typeOf){
+    if( (votes[vote_index]).typeOf != vote_type){
       change = true;
     }
 
     if(change){
-      votes[vote_index].typeOf = typeOf;
+      votes[vote_index].typeOf = vote_type;
 
-      if(typeOf == Vote_Type.For){
+      if(vote_type == Vote_Type.For){
 	con--;
 	pro++;
       }
 
-      if(typeOf == Vote_Type.Against){
+      if(vote_type == Vote_Type.Against){
 	con++;
 	pro--;
       }
     }
   }
+}
   
   
--- contracts/home.sol
+++ contracts/home.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.5.0;
 
 import  "./home_base.sol";
+import "./confirmation_prop.sol";
 
 contract Home is Home_base {
 
   
 
-  mapping(uint => Proposal) public proposals;
-  uint public propID = 0;
 
   mapping(address => Member) public member;
   address[] public mem_addr;
@@ -20,21 +19,6 @@ contract Home is Home_base {
   //assets owned by Home, in shares of itself and Ether
   uint public home_shares;
   uint public home_account;
-
-  modifier isMember {
-    bool _isMember = false;
-    
-    for(uint i=0; i<mem_addr.length; i++){
-      if(mem_addr[i] == msg.sender){
-	_isMember = true;
-	break;
-      }
-    }
-
-    require(_isMember,
-	    'only members can call this function');
-    _;
-  }
 
   modifier isConfirmed {
     require(confirmed,
@@ -79,102 +63,42 @@ contract Home is Home_base {
     emit newMember(founder_addr, founder_type);
 
     home_shares -= founder_shares;
-
-
-  }
-
-  //creates new share offer contract
-  function offerShares(uint amount) isMember public {
   }
 
   //will check that all conditions are met before changing finalize state
   function initConfirmationProp() public {
 
-    require(propTypeExists(Proposal_Type.Confirmation) == false,
-	    'A finalize vote has already been issued');
+    //    require(propTypeExists(Proposal_Type.Confirmation) == false,
+    //	    'A finalize vote has already been issued');
     require(msg.sender == mem_addr[0],
 	    'Only the contract creator can call a finalize vote');
 
-    uint id = addProposal(Proposal_Type.Confirmation);
+    //uint id = addProposal(Proposal_Type.Confirmation);
 
     //it is assumed that the original founder is in favor of approval
-    castVote(id, Vote_Type.For);
+    //castVote(id, Vote_Type.For);
   }
-
-  /*function castVote(uint id, Vote_Type typeOf) isMember public {
- 
-    require(proposals[id].result == Result.Undecided,
-	    'this vote has already been decided');
-
-    Proposal_Type propType = proposals[id].typeOf;
-
-    if(propType == Proposal_Type.Confirmation){
-      castConfirmationVote(msg.sender, id, typeOf);
-    }
-
-  }
-  
-  function castConfirmationVote(address addr, uint id, Vote_Type typeOf) internal {
-    
-    Vote memory vote;
-    vote.member = addr;
-    vote.typeOf = typeOf;
-    proposals[id].votes.push(vote);
-
-    emit voteCast(typeOf, id);
-
-    checkConfirmationProp(id);
-  }
-
-  function checkConfirmationProp(uint id) internal {
-    Proposal memory prop = proposals[id];
-
-    Vote[] memory votes = prop.votes;
-
-    uint total_members = mem_addr.length;
-    uint pro = 0;
-    
-    for(uint i=0; i<votes.length; i++){
-      if(votes[i].typeOf == Vote_Type.For) {
-	pro++;
-      }
-    } 
-    
-
-    if((pro/total_members) * 100 >= APPROVAL_PROP_THRESHOLD) {
-      prop.result = Result.Affirmed;
-      confirmed = true;
-      emit propFinished(propID, prop.typeOf, prop.result);
-      emit homeFinalized();
-
-      } 
-      }*/
-  
-  //goes through all proposals to see if a proposal of the given type exists
-  function propTypeExists(Proposal_Type typeOf) internal view returns (bool){
-
-    for(uint8 i = 0; i<propID; i++){
-      if(proposals[i].typeOf == typeOf  &&
-	 proposals[i].result== Result.Undecided )
-	return true;
-    }
-
-    return false;
-  }
-
-  function addProposal(Proposal_Type typeOf) internal returns(uint) {
-    proposals[propID].typeOf = typeOf;
-    emit newProp(typeOf, propID);
-    return propID++;
-  }
-
 
   //return current share price, with discount added if applicable too member
-  function calcSharePrice(address member) isConfirmed public returns(uint) {}
+  function calcSharePrice(address _member) isConfirmed public returns(uint) {}
 
   //create offer contract, subtract amount from creator's shares
-  function offerShares(uint amount) isMember, isConfirmed public {}
+  function offerShares(uint _amount)  isConfirmed public {}
 
+  function isMember(address _member) public returns (bool){
+    bool _isMember = false;
+    
+    for(uint i=0; i<mem_addr.length; i++){
+      if(mem_addr[i] == _member){
+	_isMember = true;
+	break;
+      }
+    }   
 
-  
+    return _isMember;
+  }
+
+  function getTotalMembers() public returns (uint){
+    return mem_addr.length;
+  }
 }
